8037d3b424b80da1376b65f971584896c79a6985
PrasadK05 <kardeprasad1996@gmail.com>
2023-05-15 18:29:31 +0530

    added updated  user routes

--- src/config/db.js
+++ src/config/db.js
@@ -7,6 +7,11 @@ const connect = () => {
   return mongoose.connect(URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+  }) .then(() => {
+    console.log('Connected to MongoDB Atlas');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB Atlas:', error);
   });
 };
 
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -12,28 +12,38 @@ const app = express.Router();
 
 app.post("/signup", async (req, res) => {
   let data = req.body;
+  // RegEx for Password, Eamil and Mobile Number
   let emailRegex =
     /^[_a-zA-Z0-9-]+(\.[_a-zA-Z0-9-]+)*@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*(\.[a-z]{2,4})$/;
   let mobNumberRegex = /^[6-9]\d{9}$/;
+  let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
+
+  // Checking values as per RegEx
   if (!emailRegex.test(data.email)) {
     return res.send({ status: false, message: "Invalid Email" });
   }
   if (!mobNumberRegex.test(data.monNumber)) {
     return res.send({ status: false, message: "Invalid Mobile Number" });
   }
+  if (!passwordRegex.test(data.password)) {
+    return res.send({ status: false, message: "Invalid Password" });
+  }
+
+  // Checking exsitance of user
   let already_exist = await User.findOne({ email: data.email });
   if (already_exist) {
-    return res.send({ status: false, message: "user already registered" });
+    return res.send({ status: false, message: "User Already Registered" });
   }
+  // Signup
   let hash = await argon2.hash(data.password);
   let user = await User.create({ ...data, password: hash });
   if (user) {
     return res.send({
       status: true,
-      messege: "user created successfully",
+      messege: "User Created Successfully",
     });
   } else {
-    return res.send({ status: false, messege: "something went wrong" });
+    return res.send({ status: false, messege: "Something Went Wrong" });
   }
 });
 
